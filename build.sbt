cdfaa83429d5214ae1409ca112a3df4eccca6525
Leon Weemen <weemen@leonweemen.nl>
2026-03-16 10:15:38 +0100

    dependency updates and more polishing

--- build.sbt
+++ build.sbt
@@ -2,17 +2,19 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.8.1"
 
-lazy val esProcessors = uri("https://github.com/weemen/es-processors.git#v0.0.1")
+lazy val esProcessors = uri("https://github.com/weemen/es-processors.git#v0.0.2")
 
 lazy val root = (project in file("."))
   .settings(
     name := "EsProcessorsSampleApp",
     libraryDependencies ++= Seq(
-      "org.apache.pekko" %% "pekko-actor-typed" % "1.1.2",
-      "org.apache.pekko" %% "pekko-actor"       % "1.1.2",
-      "org.apache.pekko" %% "pekko-stream"      % "1.1.2",
-      "org.apache.pekko" %% "pekko-slf4j"       % "1.1.2",
-      "ch.qos.logback"    % "logback-classic"   % "1.5.16"
+      "org.apache.pekko" %% "pekko-actor-typed"         % "1.1.2",
+      "org.apache.pekko" %% "pekko-actor"               % "1.1.2",
+      "org.apache.pekko" %% "pekko-stream"              % "1.1.2",
+      "org.apache.pekko" %% "pekko-slf4j"               % "1.1.2",
+      "ch.qos.logback"    % "logback-classic"           % "1.5.16",
+      "org.scalatest"    %% "scalatest"                 % "3.2.19" % Test,
+      "org.apache.pekko" %% "pekko-actor-testkit-typed" % "1.1.2"  % Test
     )
   )
   .dependsOn(ProjectRef(esProcessors, "pekko-processors-sagas"))
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,46 +1,65 @@
-import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Props, SpawnProtocol}
+import org.apache.pekko.actor.typed.{
+  ActorRef,
+  ActorSystem,
+  Props,
+  SpawnProtocol
+}
 import org.apache.pekko.actor.typed.scaladsl.AskPattern.*
 import org.apache.pekko.util.Timeout
 import processors.BaseProcessor
-import actors.{ProcessorManagerActor, RegisterProcessor, ProcessEvent, CborSerializable}
+import actors.{
+  CborSerializable,
+  ProcessEvent,
+  ProcessorManagerActor,
+  RegisterProcessor
+}
+import org.apache.pekko.Done
 
 import java.util.UUID
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
-final case class DomainEventA(myPropertyA: String, myPropertyB: Int) extends CborSerializable
-final case class DomainEventB(myPropertyX: String, myPropertyY: Int) extends CborSerializable
-final case class DomainEventC(myPropertyC: String, myPropertyD: Int) extends CborSerializable
+final case class DomainEventA(myPropertyA: String, myPropertyB: Int)
+    extends CborSerializable
+final case class DomainEventB(myPropertyX: String, myPropertyY: Int)
+    extends CborSerializable
 
-class SomeProcessorType(listOfEvents: List[Any]) extends BaseProcessor(listOfEvents):
+class SomeProcessorType(listOfEvents: List[Any])
+    extends BaseProcessor(listOfEvents):
 
-  def process(): Option[DomainEventC] = {
+  def process(): Option[Done] = {
     for
       eventA <- getEventByType[DomainEventA]
       eventB <- getEventByType[DomainEventB]
-    yield DomainEventC(myPropertyC = "C", myPropertyD = eventA.myPropertyB + eventB.myPropertyY)
+      _ = println(s"<< all events received should send new command here >>")
+    yield Done
   }
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @main
 def main(): Unit = {
-  val system: ActorSystem[SpawnProtocol.Command] = ActorSystem(SpawnProtocol(), "TaggedActorsTyped")
+  val system: ActorSystem[SpawnProtocol.Command] =
+    ActorSystem(SpawnProtocol(), "TaggedActorsTyped")
 
   val field_to_match_1 = UUID.randomUUID().toString
   val field_to_match_2 = UUID.randomUUID().toString
 
   val eventA = DomainEventA(field_to_match_1, 1)
   val eventB = DomainEventB(field_to_match_1, 2)
-  val eventC = DomainEventC(field_to_match_2, 2)
 
-  val processor = new SomeProcessorType(listOfEvents = List(classOf[DomainEventA], classOf[DomainEventB]))
+  val processor = new SomeProcessorType(listOfEvents =
+    List(classOf[DomainEventA], classOf[DomainEventB])
+  )
 
   implicit val timeout: Timeout = 3.seconds
   implicit val scheduler = system.scheduler
 
-
-
   val managerActorFuture = system.ask[ActorRef[Any]](replyTo =>
-    SpawnProtocol.Spawn(ProcessorManagerActor("processorManager"), "processorManager", Props.empty, replyTo)
+    SpawnProtocol.Spawn(
+      ProcessorManagerActor("processorManager"),
+      "processorManager",
+      Props.empty,
+      replyTo
+    )
   )
   val managerActor = Await.result(managerActorFuture, timeout.duration)
   println("Manager actor spawned")
@@ -49,4 +68,3 @@ def main(): Unit = {
   managerActor ! ProcessEvent(eventB, field_to_match_1) // Use same ID as event
   system.terminate()
 }
-
